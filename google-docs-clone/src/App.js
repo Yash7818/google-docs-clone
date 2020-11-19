c4a0f0d445a64c8142baee5cb1b6c789bfe1e0d8
ashutosh1401 <ashutoshthakur_14@yahoo.com>
2020-11-19 15:17:32 +0530

    Made Initial Editor

--- google-docs-clone/src/App.js
+++ google-docs-clone/src/App.js
@@ -1,55 +1,11 @@
-import React, {useMemo, useState, useCallback } from "react";
-import { createEditor, Editor, Transforms  } from 'slate'
-import { Slate, Editable, withReact } from 'slate-react'
+import React from "react"
+import DocEditor from "./DocEditor"
 
 const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ])
-
-  const renderElement = useCallback(props => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />
-      default:
-        return <DefaultElement {...props} />
-    }
-  }, [])
-
   return (
-    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-      <Editable
-        renderElement={renderElement}
-        onKeyDown={event => {
-          if (event.key === '`' && event.ctrlKey) {
-            // Prevent the "`" from being inserted by default.
-            event.preventDefault()
-            // Otherwise, set the currently selected blocks type to "code".
-            Transforms.setNodes(
-              editor,
-              { type: 'code' },
-              { match: n => Editor.isBlock(editor, n) }
-            )
-          }
-        }}
-      />
-    </Slate>
+    <div>
+      <DocEditor />
+    </div>
   )
-}
-
-const CodeElement = props => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  )
-}
-
-const DefaultElement = props => {
-  return <p {...props.attributes}>{props.children}</p>
 }
 export default App;
---
+++ google-docs-clone/src/editorComp.js
@@ -0,0 +1,89 @@
+import React from "react"
+import { cx,css } from '@emotion/css'
+
+export const Button = React.forwardRef(
+    (
+      {
+        className,
+        active,
+        reversed,
+        ...props
+      }
+    ) => (
+      <span
+        {...props}
+        className={cx(
+          className,
+          css`
+            cursor: pointer;
+            color: ${reversed
+              ? active
+                ? 'white'
+                : '#aaa'
+              : active
+              ? 'black'
+              : '#ccc'};
+          `
+        )}
+      />
+    )
+  )
+
+  export const Icon = React.forwardRef(
+    (
+      { className, ...props }
+    ) => (
+      <span
+        {...props}
+        className={cx(
+          'material-icons',
+          className,
+          css`
+            font-size: 18px;
+            vertical-align: text-bottom;
+          `
+        )}
+      />
+    )
+  )
+
+  export const Menu = React.forwardRef(
+    (
+      { className, ...props }
+    ) => (
+      <div
+        {...props}
+        className={cx(
+          className,
+          css`
+            & > * {
+              display: inline-block;
+            }
+            & > * + * {
+              margin-left: 15px;
+            }
+          `
+        )}
+      />
+    )
+  )
+
+  export const Toolbar = React.forwardRef(
+    (
+      { className, ...props }
+    ) => (
+      <Menu
+        {...props}
+        className={cx(
+          className,
+          css`
+            position: relative;
+            padding: 1px 18px 17px;
+            margin: 0 -20px;
+            border-bottom: 2px solid #eee;
+            margin-bottom: 20px;
+          `
+        )}
+      />
+    )
+  )
