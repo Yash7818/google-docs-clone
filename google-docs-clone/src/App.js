7b86cb4379e81c00a6c5ab55ade66f7f44f8e48b
unknown <yashwandhare1234@gmail.com>
2020-11-19 18:23:54 +0530

    router added

--- google-docs-clone/src/App.js
+++ google-docs-clone/src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import DocEditor from "./Editor/DocEditor"
 
 const App = () => {
   return (
-    <div>
-      <DocEditor />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true} component={DocEditor}></Route>
+      </Switch>
+    </Router>
   )
 }
 export default App;
