c73e1685ec6e976bf630f3e6381588cdf0ffa221
unknown <yashwandhare1234@gmail.com>
2020-11-20 11:50:53 +0530

    home page added

--- google-docs-clone/src/App.js
+++ google-docs-clone/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
-import DocEditor from "./Editor/DocEditor"
+import DocEditor from "./Editor/DocEditor";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact={true} component={DocEditor}></Route>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/editor" exact={true} component={DocEditor}></Route>
       </Switch>
     </Router>
   )
